9c7cb088e7a23eb8c373bf3c6bca9c9e22b9d059
sanathkumar <sanathkumar225@gmail.com>
2024-03-23 12:56:50 +0530

    Balance checking

---
+++ Bank/CustomerDetails.java
@@ -0,0 +1,19 @@
+package Bank;
+
+public class CustomerDetails {
+    String name="Ravi";
+    double avilAmount=34156.67;
+    long accNumber=12345678l;
+
+
+    public void transferAmount(double amount){
+        avilAmount=avilAmount-amount;
+        System.out.println(avilAmount);
+    }
+
+    public static void main(String[] args) {
+         CustomerDetails customerDetails = new CustomerDetails();
+         customerDetails.transferAmount(500);
+
+    }
+}
